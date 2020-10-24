184aa169bf3443986b5737598b6fc8b305745094
Ozaq <ozaq@posteo.de>
2020-10-24 19:01:06 +0200

    Add clang-tidy to cmake
    This commit brings a new configuration option for cmake WITH_TIDY.
    
    If WITH_TIDY is set clang tidy will be enabeled to run alongside the
    build (This is independent of the actual compiler used)
    
    clang-tidy-10 is required exactly so that there is no discrepency
    between the CI check and the check on your local machine.
    
    WITH_TIDY is enabled by default.
    
    A note about the find procedure for tidy:
    This is untested on Windows and MacOS.
    `clang-tidy` or `clang-tidy-10` binaries are accpected as long as the
    `--version` output confirms it is verion 10.x

--- cpp/cmake/helper_functions.cmake
+++ cpp/cmake/helper_functions.cmake
@@ -15,3 +15,51 @@ function(check_prefix_path)
         endforeach()
     endif()
 endfunction()
+
+# This macro ensures that there is always an explicitly set build type.
+# If CMAKE_BUILD_TYPE is NOT set during configure, CMAKE_BUILD_TYPE will
+# now default to 'Release'
+macro(enforce_build_type_is_set)
+    set(default_build_type "Release")
+    if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+    message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+    set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
+        STRING "Choose the type of build." FORCE)
+    endif()
+endmacro()
+
+# Adds an option to cmake configure to automatically build with clang
+# tidy enabled. NOTE: The clang tidy version is explicitly pinned.
+macro(add_clang_tidy_support_option)
+    option(WITH_TIDY "Build with clang tidy checks enabled (This will increase your build time)" ON)
+    if(WITH_TIDY)
+        find_program(
+            CLANG_TIDY
+            NAMES
+                clang-tidy-10
+                clang-tidy
+            REQUIRED
+        )
+        if(CLANG_TIDY)
+            execute_process(
+                COMMAND ${CLANG_TIDY} --version
+                OUTPUT_VARIABLE clang_tidy_version_output
+                ERROR_QUIET
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+            string(REGEX REPLACE
+                ".*LLVM version ([.0-9]+).*"
+                "\\1"
+                clang_tidy_version
+                "${clang_tidy_version_output}"
+            )
+            if(clang_tidy_version MATCHES "^10.*")
+                message(STATUS "Found clang-tidy ${CLANG_TIDY} [${clang_tidy_version}]")
+                set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY})
+                set(CMAKE_C_CLANG_TIDY ${CLANG_TIDY})
+            else()
+                message(FATAL_ERROR "Found clang-tidy but it reports an incorrect version: ${clang_tidy_version}")
+            endif()
+        endif()
+    endif()
+endmacro()
