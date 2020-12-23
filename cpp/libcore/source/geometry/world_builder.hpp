22a622e49b6a2ffd23f642f554768177eab65342
Jette Schumann <j.schumann@fz-juelich.de>
2020-12-23 15:03:24 +0100

    Reformats

--- cpp/libcore/source/geometry/world_builder.hpp
+++ cpp/libcore/source/geometry/world_builder.hpp
@@ -1,23 +1,23 @@
 #ifndef SIMULATOR_WORLD_BUILDER_HPP
 #define SIMULATOR_WORLD_BUILDER_HPP
 
-#include <vector>
 #include <map>
+#include <vector>
 
-struct Point{
+struct Point {
     int x;
     int y;
 };
 
-struct Line{
+struct Line {
     Point start;
     Point end;
 };
 
-struct Area{
+struct Area {
     int id;
     std::vector<Line> lines;
-    //TODO: special properties
+    // TODO: special properties
 };
 
 class WorldBuilder
@@ -27,12 +27,12 @@ private:
     std::map<int, std::vector<Area>> m_level_to_special_areas;
 
 public:
-
     WorldBuilder() = default;
 
-    //TODO: somehow check for maximum dimensions and shift if necessary (to not exceed the int range)
+    // TODO: somehow check for maximum dimensions and shift if necessary (to not exceed the int
+    // range)
     void addBoundaryLine(int p_level, std::vector<int> p_segment);
     void addSpecialArea(int p_level, int p_id, std::vector<int> p_segments);
 };
 
-#endif //SIMULATOR_WORLD_BUILDER_HPP
+#endif // SIMULATOR_WORLD_BUILDER_HPP
