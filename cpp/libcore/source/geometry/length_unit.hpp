c6e5724220ec4b6c917893dbff9bf26500b4fe39
David Haensel <d.haensel@fz-juelich.de>
2021-01-28 15:17:37 +0100

    LengthUnit implementation (#38)
    LengthUnit provides a strong type for all length unit quantities. It stores the actual unit using `double` precision floating points in the defined `RESOLUTION` (currently meter). It automatically scales the unit from the input unit to the internal storage and to the desired unit. 
    We store the quantity in a single resolution unit to avoid a lot of scaling for arithmetic operations.

---
+++ cpp/libcore/source/geometry/length_unit.hpp
@@ -0,0 +1,237 @@
+#pragma once
+
+#include "enum.hpp"
+#include "math/int_pow.hpp"
+
+#include <type_traits>
+
+namespace jps
+{
+/// Enumeration for supported length units.
+/// Underlying integer must be set to the decimal exponent relative to meter (like SI).
+enum class Units { um = -6, mm = -3, cm = -2, dm = -1, m = 0, km = 3 };
+const int DECIMAL_BASE = 10;
+
+namespace details
+{
+/// Scales the given quantity of a length unit from `from` unit to `to` unit.
+/// The used  quantity type T can be deduced automatically.
+/// If from < to the quantity is divided and truncated towards zero for integral T.
+/// Can be called like: `scaleQuantity<Units::mm, Units::cm>(10)`
+template <Units from, Units to, typename T>
+constexpr auto scaleQuantity(T p_quantity) -> T
+{
+    static_assert(
+        std::is_floating_point_v<T>,
+        "scaleQuantity should be used with floating point types only.");
+
+    const int diff_exp = toUnderlying(from) - toUnderlying(to);
+
+    if constexpr(diff_exp < 0) {
+        return p_quantity / jps::math::intPow<DECIMAL_BASE, -diff_exp>();
+    } else {
+        return p_quantity * jps::math::intPow<DECIMAL_BASE, diff_exp>();
+    }
+}
+
+
+/// Helper struct to encapsulate the params required for LengthUnit construction
+/// @tparam QuantityType floating point type used to represent the quantity
+/// @tparam Unit of the quantity
+template <typename QuantityType, Units Unit>
+struct LengthUnitParams {
+    /// The quantity for the LengthUnit creation in Unit
+    const QuantityType quantity; // NOLINT(misc-non-private-member-variables-in-classes)
+    LengthUnitParams(QuantityType p_quantity) : quantity{p_quantity} {}
+};
+} // namespace details
+
+/// Stores the quantity of a length unit
+///
+/// LengthUnit is the base unit for all spatial data structures.
+/// It represents a quantity in RESOLUTION unit.
+/// It can only be created by explicitly passing the unit of the quantity.
+/// The construction has to be done using LengthUnitParams.
+class LengthUnit
+{
+    /// Defines the unit which is used to store the quantity internally
+    const static Units RESOLUTION = Units::m;
+
+public:
+    /// Type used for the quantity.
+    using QuantityType = double;
+
+    LengthUnit() = default;
+
+    LengthUnit(LengthUnit const & p_other) = default;
+    auto operator=(LengthUnit const & p_other) -> LengthUnit & = default;
+
+    LengthUnit(LengthUnit && p_moved) noexcept = default;
+    auto operator=(LengthUnit && p_moved) noexcept -> LengthUnit & = default;
+
+    ~LengthUnit() = default;
+
+    /// Constructors using LengthUnitParams
+    ///
+    /// Template parameters in constructors have to be auto deducible.
+    /// Otherwise it is not possible to distinguish class template params and constructor template
+    /// params.
+    /// The quantity is scaled towards the RESOLUTION unit.
+    ///
+    /// @tparam Unit is the input unit of the quantity.
+    template <Units Unit>
+    LengthUnit(details::LengthUnitParams<QuantityType, Unit> const & p_params) :
+        m_quantity{details::scaleQuantity<Unit, RESOLUTION>(p_params.quantity)}
+    {
+    }
+
+    /// Retrieves the quantity in the desired Unit
+    ///
+    /// The quantity will be scaled to the target Unit.
+    ///
+    /// @tparam Unit target unit for the quantity
+    /// @returns the quantity in the target Unit
+    template <Units Unit = RESOLUTION>
+    auto get() const -> QuantityType
+    {
+        return details::scaleQuantity<RESOLUTION, Unit>(m_quantity);
+    }
+
+    auto operator+=(LengthUnit const & p_other) noexcept -> LengthUnit &
+    {
+        m_quantity += p_other.m_quantity;
+        return *this;
+    }
+
+    auto operator-=(LengthUnit const & p_other) noexcept -> LengthUnit &
+    {
+        m_quantity -= p_other.m_quantity;
+        return *this;
+    }
+
+    auto operator<(LengthUnit const & p_other) const noexcept -> bool
+    {
+        return m_quantity < p_other.m_quantity;
+    }
+
+    auto operator>(LengthUnit const & p_other) const noexcept -> bool
+    {
+        return m_quantity > p_other.m_quantity;
+    }
+
+    auto operator==(LengthUnit const & p_other) const noexcept -> bool
+    {
+        return m_quantity == p_other.m_quantity;
+    }
+
+    auto operator!=(LengthUnit const & p_other) const noexcept -> bool
+    {
+        return m_quantity != p_other.m_quantity;
+    }
+
+private:
+    /// Stores the length unit quantity stored in RESOLUTION
+    QuantityType m_quantity{};
+
+    // friend functions
+    friend auto operator-(jps::LengthUnit p_lu) -> jps::LengthUnit
+    {
+        p_lu.m_quantity = -p_lu.m_quantity;
+        return p_lu;
+    }
+
+    friend auto operator*(jps::LengthUnit p_lu, double p_scalar) -> jps::LengthUnit
+    {
+        p_lu.m_quantity *= p_scalar;
+        return p_lu;
+    }
+
+    friend auto operator/(jps::LengthUnit p_lu, double p_scalar) -> jps::LengthUnit
+    {
+        p_lu.m_quantity /= p_scalar;
+        return p_lu;
+    }
+};
+
+/// Helper for creating LengthUnit
+///
+/// @tparam Unit is the input Unit of p_quantity
+/// @param p_quantity the quantity of the LengthUnit
+/// @returns LengthUnit with the quantity
+template <Units Unit>
+auto makeLengthUnit(LengthUnit::QuantityType p_quantity) -> LengthUnit
+{
+    return LengthUnit{details::LengthUnitParams<LengthUnit::QuantityType, Unit>{p_quantity}};
+}
+
+} // namespace jps
+
+
+/// arithmetic operations
+inline auto operator+(jps::LengthUnit p_lhs, jps::LengthUnit const & p_rhs) -> jps::LengthUnit
+{
+    p_lhs += p_rhs;
+    return p_lhs;
+}
+
+inline auto operator-(jps::LengthUnit p_lhs, jps::LengthUnit const & p_rhs) -> jps::LengthUnit
+{
+    p_lhs -= p_rhs;
+    return p_lhs;
+}
+
+inline auto operator*(double p_scalar, jps::LengthUnit p_lu) -> jps::LengthUnit
+{
+    return p_lu * p_scalar;
+}
+/// User defined literals for all units
+inline auto operator"" _um(long double p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::um>(p_quantity);
+}
+inline auto operator"" _mm(long double p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::mm>(p_quantity);
+}
+inline auto operator"" _cm(long double p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::cm>(p_quantity);
+}
+inline auto operator"" _dm(long double p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::dm>(p_quantity);
+}
+inline auto operator"" _m(long double p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::m>(p_quantity);
+}
+inline auto operator"" _km(long double p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::km>(p_quantity);
+}
+
+/// User defined literals for integrals
+inline auto operator"" _um(unsigned long long p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::um>(p_quantity);
+}
+inline auto operator"" _mm(unsigned long long p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::mm>(p_quantity);
+}
+inline auto operator"" _cm(unsigned long long p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::cm>(p_quantity);
+}
+inline auto operator"" _dm(unsigned long long p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::dm>(p_quantity);
+}
+inline auto operator"" _m(unsigned long long p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::m>(p_quantity);
+}
+inline auto operator"" _km(unsigned long long p_quantity) -> jps::LengthUnit
+{
+    return jps::makeLengthUnit<jps::Units::km>(p_quantity);
+}
---
+++ cpp/libcore/test/geometry/test_length_unit.cpp
@@ -0,0 +1,284 @@
+#include "geometry/length_unit.hpp"
+
+#include <cmath>
+#include <gtest/gtest.h>
+
+TEST(LengthUnit, NonHelperConstruction)
+{
+    using namespace jps;
+    using namespace details;
+
+    // Same unit
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::km>{10.}}.get<Units::km>()),
+        10.);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::m>{10.}}.get<Units::m>()),
+        10.);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::dm>{10.}}.get<Units::dm>()),
+        10.);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::cm>{10.}}.get<Units::cm>()),
+        10.);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::mm>{10.}}.get<Units::mm>()),
+        10.);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::um>{10.}}.get<Units::um>()),
+        10.);
+
+    // Check if scaling is used in constructor
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::km>{1.}}.get<Units::m>()),
+        1e3);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::m>{1.}}.get<Units::m>()), 1.);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::dm>{1.}}.get<Units::m>()),
+        1e-1);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::cm>{1.}}.get<Units::m>()),
+        1e-2);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::mm>{1.}}.get<Units::m>()),
+        1e-3);
+
+    // Check if scaling is used in get method
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::m>{1e3}}.get<Units::km>()),
+        1);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::m>{1.}}.get<Units::m>()), 1.);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::m>{1e-1}}.get<Units::dm>()),
+        1.);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::m>{1e-2}}.get<Units::cm>()),
+        1.);
+    EXPECT_DOUBLE_EQ(
+        (LengthUnit{LengthUnitParams<LengthUnit::QuantityType, Units::m>{1e-3}}.get<Units::mm>()),
+        1.);
+}
+
+TEST(LengthUnit, makeLengthUnit)
+{
+    using namespace jps;
+    using namespace details;
+    // Same unit
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::km>(10.).get<Units::km>()), 10.);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::m>(10.).get<Units::m>()), 10.);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::dm>(10.).get<Units::dm>()), 10.);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::cm>(10.).get<Units::cm>()), 10.);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::mm>(10.).get<Units::mm>()), 10.);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::um>(10.).get<Units::um>()), 10.);
+
+    // Check if scaling is used in constructor
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::km>(1.).get<Units::m>()), 1e3);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::m>(1.).get<Units::m>()), 1.);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::dm>(1.).get<Units::m>()), 1e-1);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::cm>(1.).get<Units::m>()), 1e-2);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::mm>(1.).get<Units::m>()), 1e-3);
+
+    // Check if scaling is used in get method
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::m>(1e3).get<Units::km>()), 1.);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::m>(1.).get<Units::m>()), 1.);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::m>(1e-1).get<Units::dm>()), 1.);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::m>(1e-2).get<Units::cm>()), 1.);
+    EXPECT_DOUBLE_EQ((makeLengthUnit<Units::m>(1e-3).get<Units::mm>()), 1.);
+}
+
+TEST(LengthUnit, UserDefinedLiterals)
+{
+    EXPECT_DOUBLE_EQ((1._um).get<jps::Units::um>(), 1.);
+    EXPECT_DOUBLE_EQ((1._mm).get<jps::Units::mm>(), 1.);
+    EXPECT_DOUBLE_EQ((1._cm).get<jps::Units::cm>(), 1.);
+    EXPECT_DOUBLE_EQ((1._dm).get<jps::Units::dm>(), 1.);
+    EXPECT_DOUBLE_EQ((1._m).get<jps::Units::m>(), 1.);
+    EXPECT_DOUBLE_EQ((1._km).get<jps::Units::km>(), 1.);
+
+    // Test integral literals
+    EXPECT_DOUBLE_EQ((1_um).get<jps::Units::um>(), 1.);
+    EXPECT_DOUBLE_EQ((1_mm).get<jps::Units::mm>(), 1.);
+    EXPECT_DOUBLE_EQ((1_cm).get<jps::Units::cm>(), 1.);
+    EXPECT_DOUBLE_EQ((1_dm).get<jps::Units::dm>(), 1.);
+    EXPECT_DOUBLE_EQ((1_m).get<jps::Units::m>(), 1.);
+    EXPECT_DOUBLE_EQ((1_km).get<jps::Units::km>(), 1.);
+}
+
+TEST(LengthUnit, arithmetics)
+{
+    using namespace jps;
+
+    auto lu       = makeLengthUnit<Units::mm>(1.);
+    auto other_lu = makeLengthUnit<Units::um>(1.);
+
+    // test +=
+    lu += other_lu;
+    EXPECT_DOUBLE_EQ(lu.get<Units::um>(), 1001.);
+    EXPECT_DOUBLE_EQ(other_lu.get<Units::m>(), 1e-6);
+
+    other_lu += other_lu;
+    EXPECT_DOUBLE_EQ(other_lu.get<Units::um>(), 2.);
+
+    lu       = 1_mm;
+    other_lu = 1_mm;
+
+    // Test symmetric + operator
+    auto result = lu + other_lu;
+    EXPECT_DOUBLE_EQ(result.get<Units::mm>(), 2.);
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), 1.);
+    EXPECT_DOUBLE_EQ(other_lu.get<Units::mm>(), 1.);
+
+    result = other_lu + lu;
+    EXPECT_DOUBLE_EQ(result.get<Units::mm>(), 2.);
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), 1.);
+    EXPECT_DOUBLE_EQ(other_lu.get<Units::mm>(), 1.);
+
+    // negativ
+    result = -lu;
+    EXPECT_DOUBLE_EQ(result.get<Units::mm>(), -1.);
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), 1.);
+
+    lu = -lu;
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), -1.);
+
+    // operator -=
+    lu       = 1_mm;
+    other_lu = 1_mm;
+
+    lu -= other_lu;
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), 0.);
+    EXPECT_DOUBLE_EQ(other_lu.get<Units::mm>(), 1.);
+
+    other_lu -= other_lu;
+    EXPECT_DOUBLE_EQ(other_lu.get<Units::mm>(), 0.);
+
+    // symmetric operator-
+    lu       = 10_mm;
+    other_lu = 1_mm;
+    result   = lu - other_lu;
+    EXPECT_DOUBLE_EQ(result.get<Units::mm>(), 9.);
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), 10.);
+    EXPECT_DOUBLE_EQ(other_lu.get<Units::mm>(), 1.);
+
+    lu       = 10_mm;
+    other_lu = 1_mm;
+    result   = other_lu - lu;
+    EXPECT_DOUBLE_EQ(result.get<Units::mm>(), -9.);
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), 10.);
+    EXPECT_DOUBLE_EQ(other_lu.get<Units::mm>(), 1.);
+
+    // division by scalar
+    lu     = 10_mm;
+    result = lu / 10;
+    EXPECT_DOUBLE_EQ(result.get<Units::mm>(), 1.);
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), 10.);
+
+    result = lu / 0;
+    EXPECT_TRUE(std::isinf(result.get<Units::mm>()));
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), 10.);
+
+    // multiplication with scalar
+    lu     = 10_mm;
+    result = lu * 5;
+    EXPECT_DOUBLE_EQ(result.get<Units::mm>(), 50.);
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), 10.);
+
+    // symmetric multiplication operation
+    lu     = 10_mm;
+    result = 5 * lu;
+    EXPECT_DOUBLE_EQ(result.get<Units::mm>(), 50.);
+    EXPECT_DOUBLE_EQ(lu.get<Units::mm>(), 10.);
+}
+
+TEST(LengthUnit, comparisonOperators)
+{
+    using namespace jps;
+    using namespace details;
+
+    // ==
+    EXPECT_TRUE(1_m == 1_m);
+    EXPECT_TRUE(1.000001_m == 1000001_um);
+    EXPECT_TRUE(1_m == 1000_mm);
+    EXPECT_TRUE(1_km == 1000000000_um);
+    EXPECT_TRUE(1.0_m == 1_m);
+
+    EXPECT_FALSE(1_m == 1_cm);
+    EXPECT_FALSE(999_cm == 999.00001_cm);
+
+    // !=
+    EXPECT_TRUE(1_m != 2_m);
+    EXPECT_TRUE(1.000001_m != 1_m);
+    EXPECT_TRUE(1_m != 1_mm);
+
+    EXPECT_FALSE(100_m != 0.1_km);
+    EXPECT_FALSE(1_m != 1_m);
+
+    // <
+    EXPECT_TRUE(1._m < 2._m);
+    EXPECT_TRUE(1_mm < 1_cm);
+    EXPECT_TRUE(999_m < 1_km);
+
+    EXPECT_FALSE(2_m < 1_m);
+    EXPECT_FALSE(2_m < 1_cm);
+    EXPECT_FALSE(1_km < 999_m);
+
+    // >
+    EXPECT_TRUE(2_m > 1_m);
+    EXPECT_TRUE(2_m > 1_cm);
+    EXPECT_TRUE(1_km > 999_m);
+
+    EXPECT_FALSE(1._m > 2._m);
+    EXPECT_FALSE(1_mm > 1_cm);
+    EXPECT_FALSE(999_m > 1_km);
+}
+TEST(LengthUnit, scaleQuantity)
+{
+    using namespace jps;
+    using namespace details;
+
+    // Floating points (double)
+
+    // From km
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::km, Units::um>(1.)), 1000000000.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::km, Units::mm>(1.)), 1000000.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::km, Units::cm>(1.)), 100000.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::km, Units::dm>(1.)), 10000.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::km, Units::m>(1.)), 1000.);
+
+    // From m
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::m, Units::km>(1200.)), 1.2);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::m, Units::dm>(2.)), 20.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::m, Units::cm>(2.)), 200.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::m, Units::mm>(2.)), 2000.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::m, Units::um>(2.)), 2000000.);
+
+    // From dm
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::dm, Units::km>(20000.)), 2.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::dm, Units::m>(19.)), 1.9);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::dm, Units::cm>(1.)), 10.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::dm, Units::mm>(1.)), 100.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::dm, Units::um>(1.)), 100000.);
+
+    // From cm
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::cm, Units::km>(120000.)), 1.2);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::cm, Units::m>(100.)), 1.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::cm, Units::dm>(12.)), 1.2);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::cm, Units::mm>(1.)), 10.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::cm, Units::um>(2.)), 20000.);
+
+    // From mm
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::mm, Units::km>(5000000.)), 5.);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::mm, Units::m>(1200.)), 1.2);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::mm, Units::dm>(540.)), 5.4);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::mm, Units::cm>(78.)), 7.8);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::mm, Units::um>(5.)), 5000.);
+
+    // From um
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::um, Units::km>(1234763874.)), 1.234763874);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::um, Units::m>(1846574.)), 1.846574);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::um, Units::dm>(192837.)), 1.92837);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::um, Units::cm>(13823.)), 1.3823);
+    EXPECT_DOUBLE_EQ((scaleQuantity<Units::um, Units::mm>(1234.)), 1.234);
+}
