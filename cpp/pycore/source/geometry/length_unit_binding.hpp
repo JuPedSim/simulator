987cd1dbd3fb9525e2376e54216bf0e726bf54c3
David Haensel <d.haensel@fz-juelich.de>
2021-02-03 12:08:11 +0100

    removes length unit binding from this PR

--- cpp/pycore/source/geometry/length_unit_binding.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <pybind11/pybind11.h>
-
-void bind_length_unit(pybind11::module_ & m);
--- cpp/pycore/source/jpscore_binding.cpp
+++ cpp/pycore/source/jpscore_binding.cpp
@@ -1,4 +1,3 @@
-#include "geometry/length_unit_binding.hpp"
 #include "geometry/world_builder_binding.hpp"
 #include "logging_binding.hpp"
 #include "simulation_binding.hpp"
@@ -18,5 +17,4 @@ PYBIND11_MODULE(jpscore, m)
     /// SUB MODULE geometry
     auto m_geometry = m.def_submodule("geometry");
     bind_world_builder(m_geometry);
-    bind_length_unit(m_geometry);
 }
