604630e75393f2c3b8bbfc8816153f7319c1fefb
schroedtert <t.schroedter@fz-juelich.de>
2021-02-03 10:56:25 +0100

    Remove obsolete dummy test

--- jps/jupedsim/tests/test_dummy.py
+++
@@ -1,8 +0,0 @@
-"""
-pytest will emit error code 5 if there was no test run.
-For the CI we use this dummy test to check if the test infrastructure is working.
-"""
-
-
-def test_dummy_CI():
-    pass
