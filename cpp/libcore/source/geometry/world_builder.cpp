b44b37f4f498b80236bf5ff2d75cf7dd6c8195d7
Jette Schumann <j.schumann@fz-juelich.de>
2021-02-03 11:13:27 +0100

    Updates changes in geometry

--- cpp/libcore/source/geometry/world_builder.cpp
+++ cpp/libcore/source/geometry/world_builder.cpp
@@ -2,26 +2,12 @@
 
 #include <utility>
 
-auto WorldBuilder::addBoundaryLine(LineSegment p_segment) -> void
+auto WorldBuilder::addBoundaryLine(const jps::LineSegment &p_segment) -> void
 {
-    if(m_level_to_boundary_lines.find(p_segment.getLevel()) != m_level_to_boundary_lines.end()) {
-        // push back if not empty
-        m_level_to_boundary_lines[p_segment.getLevel()].push_back(p_segment);
-    } else {
-        // create new vector
-        m_level_to_boundary_lines[p_segment.getLevel()] = std::vector<Line>{p_segment};
-    }
+    // TODO: ensure that coordinates belong to same level, add to map
 }
 
-auto WorldBuilder::addSpecialArea(SpecialArea p_area) -> void
+auto WorldBuilder::addSpecialArea(const jps::SpecialArea &p_area) -> void
 {
-    // TODO: check if area id at given level already exists
-
-    if(m_level_to_special_areas.find(p_area.getLevel()) != (m_level_to_special_areas.end())) {
-        // push back if not empty
-        m_level_to_special_areas[p_area.getLevel()].push_back(p_area);
-    } else {
-        // create new vector
-        m_level_to_special_areas[p_area.getLevel()] = std::vector<SpecialArea>{p_area};
-    }
+    // TODO: check if area id at given level already exists, add to map
 }
--- cpp/libcore/source/geometry/world_builder.hpp
+++ cpp/libcore/source/geometry/world_builder.hpp
@@ -1,7 +1,6 @@
 #ifndef SIMULATOR_WORLD_BUILDER_HPP
 #define SIMULATOR_WORLD_BUILDER_HPP
 
-#include "coordinate.hpp"
 #include "line_segment.hpp"
 #include "special_area.hpp"
 
@@ -11,17 +10,14 @@
 class WorldBuilder
 {
 private:
-    std::map<int, std::vector<LineSegment>> m_level_to_boundary_lines;
-    std::map<int, std::vector<SpecialArea>> m_level_to_special_areas;
+    std::map<int, std::vector<jps::LineSegment>> m_level_to_boundary_lines;
+    std::map<int, std::vector<jps::SpecialArea>> m_level_to_special_areas;
 
 public:
     WorldBuilder() = default;
 
-    // TODO: somehow check for maximum dimensions and shift if necessary (to not exceed the int
-    // range)
-
-    void addBoundaryLine(LineSegment p_segment);
-    void addSpecialArea(SpecialArea p_area);
+    void addBoundaryLine(const jps::LineSegment &p_segment);
+    void addSpecialArea(const jps::SpecialArea &p_area);
 };
 
 #endif // SIMULATOR_WORLD_BUILDER_HPP
