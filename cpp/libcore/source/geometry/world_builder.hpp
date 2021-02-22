4ad4b4758e739444efcd427b1bb763fd36cadc8e
Jette Schumann <j.schumann@fz-juelich.de>
2021-02-22 11:57:52 +0100

    Reformats

--- cpp/libcore/source/geometry/world_builder.hpp
+++ cpp/libcore/source/geometry/world_builder.hpp
@@ -16,7 +16,7 @@ private:
     std::map<jps::Level, std::vector<jps::SpecialArea>> m_level_to_special_areas;
 
 public:
-    WorldBuilder()  = default;
+    WorldBuilder() = default;
 
     auto addLineSegment(const jps::Level & p_level, const jps::LineSegment & p_segment) -> void;
     auto addSpecialArea(const jps::Level & p_level, const jps::SpecialArea & p_area) -> void;
