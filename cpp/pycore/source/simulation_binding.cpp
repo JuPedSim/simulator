82884640950cc1e25875932a3ad821307119b5d1
Ozaq <ozaq@posteo.de>
2021-05-28 13:45:39 +0200

    Expose compute_next_step in python
    Prototypical 'compute_next_step' function now exposed in the python
    wrapper.

--- cpp/pycore/source/simulation_binding.cpp
+++ cpp/pycore/source/simulation_binding.cpp
@@ -7,5 +7,6 @@ void bind_simulation(pybind11::module_ & m)
 {
     pybind11::class_<jps::Simulation>(m, "Simulation")
         .def(pybind11::init<>())
-        .def("get", &jps::Simulation::getValue);
+        .def("get", &jps::Simulation::getValue)
+        .def("compute_next_step", &jps::Simulation::computeNextStep);
 }
--- cpp/pycore/test/import_test.py
+++ cpp/pycore/test/import_test.py
@@ -10,3 +10,13 @@ def test_import():
         import jpscore
     except:
         pytest.fail("Could not import jpscore!")
+
+
+def test_simulator_interface_has_compute_next_step():
+    """
+    Make sure that all methods that we want to expose are actually callable.
+    """
+    import jpscore
+
+    sim = jpscore.Simulation()
+    assert callable(getattr(sim, "compute_next_step"))
