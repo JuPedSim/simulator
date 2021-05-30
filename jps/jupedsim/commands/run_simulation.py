d48585ade872e9c7d2e529c7a92d98d4fd10df7e
Ozaq <ozaq@posteo.de>
2021-05-30 12:44:44 +0200

    Add reading of spawn events
    Spawn pedestrian events are now read from 'events.json' and processed in
    the repective iteration. Each SpawnPedestrian event creates a new
    pedestrian in the simulation at the appropriate coordinates.
    
    Since there is no backing logic yet, no error checking e.g. collision
    detection, level plausability etc. is done.
    
    But the integration should work now in so far as that we can add Agents
    to the system.
    
    This commit also fixes a bug in the generate-pedestrian tool, now
    '-overwrite' shoudl work as advertised.

--- jps/jupedsim/commands/run_simulation.py
+++ jps/jupedsim/commands/run_simulation.py
@@ -1,6 +1,10 @@
+from collections import deque
 from pathlib import Path
 
 from jpscore import Simulation
+from jpscore.geometry import Coordinate, LengthUnit, Level, Units
+from jupedsim.util.event import SpawnPedestrianEvent
+from jupedsim.util.generator import read_events
 from jupedsim.util.loghelper import log_debug, log_error, log_info
 from jupedsim.util.simulation_files import SimulationFiles
 
@@ -32,14 +36,30 @@ class RunSimulationCommand:
             log_error(f"Error gathering required files: {e}")
             return 0
         self.simulation = Simulation()
+        self.events = deque(read_events(self.simulation_files.events_json))
+
         for iteration in range(args.iterations):
             log_info(f"Simulating iteration {iteration}")
-            log_debug("Processing events")
-            self.process_events()
+            self._process_events(iteration)
             log_debug("Updating agents")
             self.simulation.compute_next_step()
         return 0
 
-    def process_events(self) -> None:
-        # TODO
-        pass
+    def _process_events(self, current_iteration: int) -> None:
+        log_debug("Processing events")
+        while self.events and self.events[0].time <= current_iteration:
+            event = self.events.popleft()
+            log_info(f"{event}")
+            if isinstance(event.event, SpawnPedestrianEvent):
+                log_debug(
+                    f"Adding agent to simulation at {event.event.position}"
+                )
+                # TODO (kkz) The SpawnPedestrianEvent should already contian jpscore.Coordinates.
+                # It feels way to verbose to create them here
+                self.simulation.add_agent(
+                    Coordinate(
+                        LengthUnit(event.event.position[0], Units.m),
+                        LengthUnit(event.event.position[1], Units.m),
+                        Level(event.event.level),
+                    )
+                )
