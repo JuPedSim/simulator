895461dd9df44a771b0e5929abb2ae52f884f3f4
David Haensel <david@davidscorner.de>
2021-06-07 12:32:14 +0200

    Removes WorldBuilder in favor of World itself
    At the moment it doesn't make sense to keep WorldBuilder and World at
    the same time. For the WorldBuilder::build method we would copy
    everything to World and initialize. This can be done in one World.
    Since we can not move the World from Python to C++ we need to create an
    empty World in C++ Simulation upfront. The C++ code should be the owner
    of central objects. The World is returned by reference to pybind. This
    World can then be filled with geometry structures.

--- cpp/libcore/source/geometry/world.hpp
+++ cpp/libcore/source/geometry/world.hpp
@@ -8,21 +8,21 @@
 
 namespace jps
 {
-class WorldBuilder
+class World
 {
 private:
     std::map<jps::Level, std::vector<jps::LineSegment>> m_level_to_line_segments;
     std::map<jps::Level, std::vector<jps::SpecialArea>> m_level_to_special_areas;
 
 public:
-    WorldBuilder()  = default;
-    ~WorldBuilder() = default;
+    World()  = default;
+    ~World() = default;
 
-    WorldBuilder(const WorldBuilder &) = delete;
-    auto operator=(const WorldBuilder &) -> WorldBuilder & = delete;
+    World(const World &) = delete;
+    auto operator=(const World &) -> World & = delete;
 
-    WorldBuilder(WorldBuilder &&) = default;
-    auto operator=(WorldBuilder &&) -> WorldBuilder & = default;
+    World(World &&) = default;
+    auto operator=(World &&) -> World & = default;
 
     auto addLineSegment(const jps::Level & p_level, const jps::LineSegment & p_segment) -> void;
     auto addSpecialArea(const jps::Level & p_level, const jps::SpecialArea & p_area) -> void;
--- cpp/pycore/source/simulation_binding.cpp
+++ cpp/pycore/source/simulation_binding.cpp
@@ -1,6 +1,6 @@
 #include "simulation_binding.hpp"
 
-#include "geometry/world_builder.hpp"
+#include "geometry/world.hpp"
 
 #include <memory>
 #include <pybind11/pybind11.h>
@@ -9,13 +9,12 @@
 void bind_simulation(pybind11::module_ & m)
 {
     pybind11::class_<jps::Simulation>(m, "Simulation")
-        .def(pybind11::init([] {
-            return std::make_unique<jps::Simulation>(std::make_unique<jps::WorldBuilder>());
-        }))
+        .def(pybind11::init(
+            [] { return std::make_unique<jps::Simulation>(std::make_unique<jps::World>()); }))
         .def("compute_next_step", &jps::Simulation::computeNextStep)
         .def("add_agent", &jps::Simulation::addAgent)
         .def(
-            "get_world_builder",
-            &jps::Simulation::getWorldBuilder,
+            "get_world",
+            &jps::Simulation::getWorld,
             pybind11::return_value_policy::reference_internal);
 }
