6fbe74499a7ba607615fe2441c6faf9fdf458fbc
schroedtert <t.schroedter@fz-juelich.de>
2021-06-17 09:10:53 +0200

    Add unique_id to pedestrian class
    Pedestrian class has now a constructor and holds an unique_id. Additionally binding for the pedestrian unique_id has been created.
    
    Signed-off-by: schroedtert <t.schroedter@fz-juelich.de>

---
+++ cpp/pycore/source/util/unique_id_binding.cpp
@@ -0,0 +1,24 @@
+#include "unique_id_binding.hpp"
+
+#include <agent/agent.hpp>
+#include <pybind11/operators.h>
+#include <util/unique_id.hpp>
+
+void bind_unique_id(pybind11::module_ & m)
+{
+    pybind11::class_<jps::UniqueId<jps::Agent>>(m, "UniqueIDAgent")
+        .def(pybind11::init<>())
+        .def_property_readonly("id", &jps::UniqueId<jps::Agent>::getID)
+        // NOLINTNEXTLINE(misc-redundant-expression)
+        .def(pybind11::detail::self == pybind11::detail::self)
+        // NOLINTNEXTLINE(misc-redundant-expression)
+        .def(pybind11::detail::self != pybind11::detail::self)
+        .def(
+            "__str__",
+            [](const jps::UniqueId<jps::Agent> & id) {
+                return fmt::format(FMT_STRING("{}"), id);
+            })
+        .def("__repr__", [](const jps::UniqueId<jps::Agent> & id) {
+            return fmt::format(FMT_STRING("{}"), id);
+        });
+}
---
+++ cpp/pycore/test/agent/agent_test.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+def test_agent_interface_has_bindings():
+    """
+    Make sure that all methods that we want to expose are actually callable.
+    """
+    import jpscore
+    from jpscore.geometry import Coordinate, Level, LengthUnit, Units
+
+    agent = jpscore.Agent(
+        Coordinate(
+            LengthUnit(94.45, Units.m),
+            LengthUnit(90941, Units.m),
+            Level(-912),
+        )
+    )
+    assert hasattr(agent, "id")
+    assert hasattr(agent, "pos")
