6873e3f8da57432fc5133f1b69c4f9216f85bd67
Jette Schumann <j.schumann@fz-juelich.de>
2021-07-22 11:51:48 +0200

    Add bounding box to level storage

--- cpp/libcore/source/geometry/level_storage.cpp
+++ cpp/libcore/source/geometry/level_storage.cpp
@@ -5,16 +5,37 @@ namespace jps
 auto LevelStorage::addLineSegment(LineSegment const & p_segment) -> void
 {
     m_line_segments.push_back(p_segment);
+    this->updateBoundingBox(p_segment.getStart());
+    this->updateBoundingBox(p_segment.getEnd());
 }
 
 auto LevelStorage::addSpecialArea(SpecialArea const & p_area) -> void
 {
     m_special_areas.push_back(p_area);
+    for(auto const & coordinate : p_area.getArea().getPolygon()) {
+        this->updateBoundingBox(coordinate);
+    }
 }
 
 auto LevelStorage::addAgent(Coordinate const & p_coordinate) -> void
 {
     m_agents.emplace_back(Agent{p_coordinate});
+    this->updateBoundingBox(p_coordinate);
+}
+
+auto LevelStorage::updateBoundingBox(const Coordinate & p_coordinate) -> void
+{
+    if(p_coordinate.x < m_bounding_box.lower_left.x) {
+        m_bounding_box.lower_left.x = p_coordinate.x;
+    } else if(p_coordinate.x > m_bounding_box.upper_right.x) {
+        m_bounding_box.upper_right.x = p_coordinate.x;
+    }
+
+    if(p_coordinate.y < m_bounding_box.lower_left.y) {
+        m_bounding_box.lower_left.y = p_coordinate.y;
+    } else if(p_coordinate.y > m_bounding_box.upper_right.y) {
+        m_bounding_box.upper_right.y = p_coordinate.y;
+    }
 }
 
 } // namespace jps
--- cpp/libcore/source/geometry/level_storage.hpp
+++ cpp/libcore/source/geometry/level_storage.hpp
@@ -6,14 +6,31 @@
 #include "geometry/line_segment.hpp"
 #include "geometry/special_area.hpp"
 
+#include <limits>
 #include <vector>
 namespace jps
 {
+struct BoundingBox {
+    Coordinate lower_left;  // NOLINTLINE(cppcoreguidelines-non-private-member-variables-in-classes)
+    Coordinate upper_right; // NOLINTLINE(cppcoreguidelines-non-private-member-variables-in-classes)
+
+    [[maybe_unused]] BoundingBox(Coordinate p_lower_left, Coordinate p_upper_right) :
+        lower_left(p_lower_left), upper_right(p_upper_right){};
+};
+
 class LevelStorage
 {
     std::vector<LineSegment> m_line_segments;
     std::vector<SpecialArea> m_special_areas;
     std::vector<Agent> m_agents;
+
+    BoundingBox m_bounding_box{
+        Coordinate(
+            makeLengthUnit<Units::m>(std::numeric_limits<double>::infinity()),
+            makeLengthUnit<Units::m>(std::numeric_limits<double>::infinity())),
+        Coordinate(
+            makeLengthUnit<Units::m>(-std::numeric_limits<double>::infinity()),
+            makeLengthUnit<Units::m>(-std::numeric_limits<double>::infinity()))};
 
 public:
     auto addLineSegment(LineSegment const & p_segment) -> void;
@@ -21,5 +38,9 @@ public:
     auto getAgents() -> std::vector<Agent> & { return m_agents; };
     auto getAgents() const -> std::vector<Agent> const & { return m_agents; };
     auto addAgent(Coordinate const & p_coordinate) -> void;
+    auto getBoundingBox() -> BoundingBox const & { return m_bounding_box; };
+
+private:
+    auto updateBoundingBox(Coordinate const & p_coordinate) -> void;
 };
 } // namespace jps
