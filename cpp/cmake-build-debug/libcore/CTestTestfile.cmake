e694c9da6da2cb8e27761e4be3ec768f4bdb2df1
Rober Köten <74518251+roberko44@users.noreply.github.com>
2021-09-12 18:05:34 +0200

    Revert "Delete build files"
    This reverts commit 2b55e2a657d54a51a46b3a427eb7169e4adfdca4.

---
+++ cpp/cmake-build-debug/libcore/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/fyxy/Schreibtisch/simulator/cpp/libcore
+# Build directory: /home/fyxy/Schreibtisch/simulator/cpp/cmake-build-debug/libcore
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("source")
+subdirs("test")
---
+++ cpp/cmake-build-debug/libcore/source/CMakeFiles/core.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "../../lib/libcore.a"
+  "../../lib/libcore.pdb"
+  "CMakeFiles/core.dir/geometry/area.cpp.o"
+  "CMakeFiles/core.dir/geometry/coordinate.cpp.o"
+  "CMakeFiles/core.dir/geometry/helper/polygon_helper.cpp.o"
+  "CMakeFiles/core.dir/geometry/level.cpp.o"
+  "CMakeFiles/core.dir/geometry/level_storage.cpp.o"
+  "CMakeFiles/core.dir/geometry/line_segment.cpp.o"
+  "CMakeFiles/core.dir/geometry/spatial_vector.cpp.o"
+  "CMakeFiles/core.dir/geometry/special_area.cpp.o"
+  "CMakeFiles/core.dir/geometry/world.cpp.o"
+  "CMakeFiles/core.dir/log.cpp.o"
+  "CMakeFiles/core.dir/neighborhood/neighborhood_search.cpp.o"
+  "CMakeFiles/core.dir/simulation.cpp.o"
+  "CMakeFiles/core.dir/tactical/shortest_path_tactical.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
