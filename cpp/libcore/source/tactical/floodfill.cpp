85473cb4b5f539e5377069b4d78dbfdf83c91f1d
Rober Köten <74518251+roberko44@users.noreply.github.com>
2021-09-22 20:21:18 +0200

    for each in floodfill and getter in level_storage

--- cpp/libcore/source/tactical/floodfill.cpp
+++ cpp/libcore/source/tactical/floodfill.cpp
@@ -8,9 +8,20 @@ jps::Floodfill::Floodfill() {
 }
 
 auto jps::Floodfill::computeGrid(jps::Simulation const & p_simulation, jps::Agent const & p_agent){
-    Grid2D</*which typename? We don't want agents here*/> flood_grid;
-    auto pos_x = p_agent.pos.x;
-    auto pos_y = p_agent.pos.y;
 
-    return flood_grid;
+    World w = p_simulation.getWorld();
+
+
+    for (LevelStorage ls: w.getLevelStorages()){
+        for(SpecialArea specialArea: ls.getSpecialAreas()){
+            vector<Coordinate> &area = specialArea.getArea().getPolygon();
+
+
+
+        }
+
+    }
+
+
+    return m_flood_grid;
 }
